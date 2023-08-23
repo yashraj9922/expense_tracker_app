778da1e7b08a44a51852e7efbaaf168078dce006
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-23 11:27:36 +0530

    basic ui for expenses section build

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:expense_tracker_app/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
