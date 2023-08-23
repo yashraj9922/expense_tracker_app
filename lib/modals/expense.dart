fbdd7a85d5d3213c771b218c5f254ae23a04f07e
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-24 01:03:52 +0530

    enum

--- lib/modals/expense.dart
+++ lib/modals/expense.dart
@@ -2,15 +2,20 @@ import 'package:uuid/uuid.dart';
 // uuid helps in generating unique IDs
 
 const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+// allows to create custom type....combination of predefined allowed values
 class Expense {
   Expense({
     required this.date,
     required this.amount,
     required this.title,
+    required this.category,
   }) : id = uuid.v4();// in dart "initializer Lists" can be used to initialize class properties (like "id") with values that are not receieved by constructor function arguments 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
   // need to build new id whenever the new Expense parameter is created...uuid(package in flutter)
 }
