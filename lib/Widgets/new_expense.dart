069921204242ee4266f4cb38b540a361dacb80ef
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-09-01 12:13:49 +0530

    title controllor

--- lib/Widgets/new_expense.dart
+++ lib/Widgets/new_expense.dart
@@ -8,24 +8,45 @@ class NewExpense extends StatefulWidget {
   }
 }
 
+// var _enteredTitle = '';
+// void _saveTitleInput(String inputValue) {
+//   _enteredTitle = inputValue;
+// }
+
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           // Taking input from user
           TextField(
+            // onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50, // maximum input text length
 
             // keyboardType: TextInputType.name, --> determines which virtual keyboard should be open when user taps the Textfield
 
             // adding label and decoration
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text("Title"),
             ),
           ),
+          ElevatedButton(
+            onPressed: () {
+              // print(_enteredTitle);
+              print(_titleController.text);
+            },
+            child: const Text("Submit"),
+          )
         ],
       ),
     );
